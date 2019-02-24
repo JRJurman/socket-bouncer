b8a1340441b90aac4a0a13f888538536a3dd893a
JRJurman <jrjurman@gmail.com>
2019-02-24 03:32:49 -0500

    initial service logic

---
+++ service.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const expressWs = require('express-ws')
+const internalIp = require('internal-ip')
+
+const wsOptions = { clientTracking: true }
+const WS = expressWs(express(), null, { wsOptions })
+const server = WS.getWss()
+const app = WS.app
+
+// TODO, add CORS
+// https://expressjs.com/en/resources/middleware/cors.html
+
+app.ws('*', (ws, req) => {
+  ws.on('message', (msg) => {
+    Array.from(server.clients)
+      .filter(client => client.upgradeReq.url === ws.upgradeReq.url)
+      .forEach(client => client.send(msg))
+  })
+})
+
+const ip = internalIp()
+const port = 5150
+
+app.listen(port, () => {
+  console.log(`Service running on  ${ip}:${port}`)
+})
